607847152015ec30646d8fd105f6dc837fdc8cf9
dr-Breachs <97856304+dr-Breachs@users.noreply.github.com>
2023-12-06 17:26:01 -0300

    Update and rename README.md to README.cpp

---
+++ README.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include <iostream>; 
+
+string description = "Hello. My name is Nicolás, but I'm fine with Breachs. I'm an Argentinian Systems Engineering student at UTN. I'm fluent in english and spanish, and I'm learning some chinese too. I mostly work with C++, but I also can also use JavaScript. I'm interested in Game Development, and have been learning to use Godot and GDScript. I hope to get a job in the field soon to start building experience and recognition.";
+
+void _showDescription(string description) {
+	cout << description << endl;
+	return;
+}
+
+int main() {
+	_showDescription(description);
+	return 0;
+}
