b891d14df5eb9315f756fd9bc052ddea1596e067
Nico <nimustafa03@gmail.com>
2025-03-28 20:11:24 -0300

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -1,7 +1,7 @@
 using namespace std;
 #include <iostream>; 
 
-string description = "Hello. My name is Nicolás, but I'm fine with Breachs. I'm an Argentinian Systems Engineering student at UTN. I'm fluent in english and spanish, and I'm learning some chinese too. I mostly work with C++, but I also can also use JavaScript. I'm interested in Game Development, and have been learning to use Godot and GDScript. I hope to get a job in the field soon to start building experience and recognition.";
+string description = "Hello. My name is Nicolás. I'm an Argentinian Systems Engineering student at UTN. I'm fluent in english and spanish, and I'm learning some chinese too. I mostly work with C++, but I also can also use JavaScript. I'm interested in Game Development, and have been learning to use Godot and GDScript. I hope to get a job in the field soon to start building experience and recognition.";
 
 void _showDescription(string description) {
 	cout << description << endl;
